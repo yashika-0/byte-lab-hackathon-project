346eb50982805a7ee2c623036f066c38534cb362
Tamanna Thakur <tamannardthakur@gmail.com>
2026-08-08 09:09:19 +0530

    Add landing page API

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/api/landing", (req, res) => {
+    const data = fs.readFileSync("./data/landing.json", "utf8");
+    const landingData = JSON.parse(data);
+
+    res.json(landingData);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
