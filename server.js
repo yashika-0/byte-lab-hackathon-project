eba6edced85fda76483a580a823ff00d139a91cc
Yashika Singh <Yashikasingh200@gmail.com>
2026-08-08 16:22:51 +0530

    Configure backend for deployment

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get("/api/landing", (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
